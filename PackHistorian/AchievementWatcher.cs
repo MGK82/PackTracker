951b5e4176e876e626c9d581b6ac55e02d9ba5b7
M. Kapma <marcel_kapma@web.de>
2017-05-27 19:06:27 +0200

    init

---
+++ PackHistorian/AchievementWatcher.cs
@@ -0,0 +1,27 @@
+﻿using HearthMirror.Objects;
+using Hearthstone_Deck_Tracker.Hearthstone;
+using HDTCard = Hearthstone_Deck_Tracker.Hearthstone.Card;
+using HearthWatcher;
+using HearthWatcher.EventArgs;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace PackHistorian {
+  class AchievementsWatcher {
+    public AchievementsWatcher() {
+      Watchers.PackWatcher.NewPackEventHandler += NewPack;
+      Watchers.PackWatcher.Run();
+    }
+
+    private void NewPack(object sender, PackEventArgs e) {
+
+      string msg = e.PackId.ToString();
+      foreach(var Card in e.Cards) {
+        HDTCard cardFromId = Database.GetCardFromId(Card.Id);
+        msg += "\n" + cardFromId.Name + " (" + cardFromId.LocalizedName + ")";
+      }
+
+      MessageBox.Show(msg);
+    }
+  }
+}
---
+++ PackHistorian/Plugin.cs
@@ -0,0 +1,72 @@
+﻿
+using Hearthstone_Deck_Tracker.Plugins;
+using System;
+using System.Reflection;
+using System.Windows.Controls;
+
+namespace PackHistorian {
+  public class Plugin : IPlugin {
+    private AchievementsWatcher _watcher;
+
+    public string Author
+    {
+      get
+      {
+        return "DBqFetti <dbqfetti@gmail.com>";
+      }
+    }
+
+    public string ButtonText
+    {
+      get
+      {
+        return "Pack Historian";
+      }
+    }
+
+    public string Description
+    {
+      get
+      {
+        return "";
+      }
+    }
+
+    public MenuItem MenuItem
+    {
+      get
+      {
+        return null;
+      }
+    }
+
+    public string Name
+    {
+      get
+      {
+        return "Pack Historian";
+      }
+    }
+
+    public Version Version
+    {
+      get
+      {
+        return Assembly.GetEntryAssembly().GetName().Version;
+      }
+    }
+
+    public void OnButtonPress() {
+    }
+
+    public void OnLoad() {
+      _watcher = new AchievementsWatcher();
+    }
+
+    public void OnUnload() {
+    }
+
+    public void OnUpdate() {
+    }
+  }
+}
