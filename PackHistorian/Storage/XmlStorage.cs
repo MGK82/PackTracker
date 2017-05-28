4aee74e14d809972424d6c12a6784aef9e1b7a20
M. Kapma <marcel_kapma@web.de>
2017-05-28 23:00:33 +0200

    Entwickelt Fetch Methode für Xml Storage.

--- PackHistorian/Storage/XmlStorage.cs
+++ PackHistorian/Storage/XmlStorage.cs
@@ -7,11 +7,58 @@ using System.Xml;
 using PackHistorian.Entity;
 using Hearthstone_Deck_Tracker;
 using System.IO;
+using Hearthstone_Deck_Tracker.Hearthstone;
+using HDTCard = Hearthstone_Deck_Tracker.Hearthstone.Card;
 
 namespace PackHistorian.Storage {
   class XmlStorage : IStorage {
     public History Fetch() {
-      throw new NotImplementedException();
+      History History = new History();
+
+      string path = Path.Combine(Config.AppDataPath, "PackHistorian", "History.xml");
+      if(File.Exists(path)) {
+        XmlDocument Xml = new XmlDocument();
+        Xml.Load(path);
+        XmlNode Root = Xml.SelectSingleNode("history");
+
+        if(Root != null) {
+          XmlNodeList Packs = Root.SelectNodes("pack");
+
+          if(Packs.Count > 0) {
+            foreach(XmlNode Pack in Packs) {
+
+                if(int.TryParse(Pack.Attributes["id"]?.Value, out int packId) && long.TryParse(Pack.Attributes["time"]?.Value, out long ticks)) {
+                  DateTime Time = new DateTime(ticks);
+                  XmlNodeList Cards = Pack.SelectNodes("card");
+
+                if(Cards.Count > 0) {
+                  List<Entity.Card> HistoryCards = new List<Entity.Card>();
+
+                  foreach(XmlNode Card in Cards) {
+                    string cardId = Card.Attributes["id"]?.Value;
+                    if(!string.IsNullOrEmpty(cardId)) {
+                      HDTCard HDTCard = Database.GetCardFromId(cardId);
+                      string premium = Card.Attributes["premium"]?.Value;
+
+                      HistoryCards.Add(new Entity.Card(HDTCard, premium == "premium"));
+                    } else {
+                      return new History();
+                    }
+                  }
+
+                  History.Add(new Pack(packId, Time, HistoryCards));
+                } else {
+                  return new History();
+                }
+              } else {
+                return new History();
+              }
+            }
+          }
+        }
+      }
+
+      return History;
     }
 
     public void Store(History History) {
@@ -33,7 +80,7 @@ namespace PackHistorian.Storage {
         PackId.Value = Pack.Id.ToString();
         PackNode.Attributes.Append(PackId);
 
-        foreach(Card Card in Pack.Cards) {
+        foreach(Entity.Card Card in Pack.Cards) {
           XmlNode CardNode = Xml.CreateElement("card");
           PackNode.AppendChild(CardNode);
 
