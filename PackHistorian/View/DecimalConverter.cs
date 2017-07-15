3d4e7c4c16478ec3cfe11edfdadf6d89ecf0b07c
M. Kapma <marcel_kapma@web.de>
2017-07-15 19:01:25 +0200

    Integer Values are noe represented in the cultural format of the setted language

---
+++ PackHistorian/View/DecimalConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using Hearthstone_Deck_Tracker;
+
+namespace PackTracker.View {
+  class DecimalConverter : IValueConverter {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+      if(int.TryParse(value.ToString(), out int dec)) {
+        CultureInfo cult = null as CultureInfo;
+        try {
+          string lang = Config.Instance.SelectedLanguage.Insert(2, "-");
+          cult = new CultureInfo(lang);
+        }
+        catch(CultureNotFoundException) {
+          cult = CultureInfo.InstalledUICulture;
+        }
+
+        return dec.ToString("n0", cult);
+      }
+
+      return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+      throw new NotImplementedException();
+    }
+  }
+}
