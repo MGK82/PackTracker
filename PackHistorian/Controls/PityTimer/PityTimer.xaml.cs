639d2c677a0e1c41026144c92fa591865f84dcc6
M. Kapma <marcel_kapma@web.de>
2017-07-23 20:01:39 +0200

    Creates Window

---
+++ PackHistorian/Controls/PityTimer/PityTimer.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PackTracker.Controls.PityTimer {
+  /// <summary>
+  /// Interaktionslogik für PityTimer.xaml
+  /// </summary>
+  public partial class PityTimer : Window {
+    public PityTimer() {
+      InitializeComponent();
+    }
+  }
+}
