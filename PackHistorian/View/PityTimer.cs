211b494c410dad0e00d46ec4da8e60b0a781bdd9
M. Kapma <marcel_kapma@web.de>
2017-07-30 16:49:31 +0200

    PityTimer reworked

--- PackHistorian/View/PityTimer.cs
+++ PackHistorian/View/PityTimer.cs
@@ -6,13 +6,15 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HearthDb.Enums;
 using PackTracker.Entity;
 
 namespace PackTracker.View {
   public class PityTimer : INotifyPropertyChanged {
     int _packId;
     bool _waitForFirst;
-    Func<Pack, bool> _condition;
+    Rarity _rarity;
+    bool _premium;
 
     int _current = 0;
     ObservableCollection<int> _prev = new ObservableCollection<int>();
@@ -21,10 +23,11 @@ namespace PackTracker.View {
     public ObservableCollection<int> Prev { get => _prev; }
     public int? Average { get => _prev.Count > 0 ? (int?)Math.Round(_prev.Average(), 0) : null; }
 
-    public PityTimer(History History, int packId, Func<Pack, bool> Condition, bool skipFirst) {
+    public PityTimer(History History, int packId, Rarity rarity, bool premium, bool skipFirst) {
       _packId = packId;
+      _rarity = rarity;
+      _premium = premium;
       _waitForFirst = skipFirst;
-      _condition = Condition;
 
       foreach(Pack Pack in History) {
         AddPack(Pack);
@@ -44,19 +47,25 @@ namespace PackTracker.View {
         return;
       }
 
-      _current++;
+      if(Condition(Pack)) {
+        int newCurr = _current;
+        _current = 0;
 
-      if(_condition(Pack)) {
         if(_waitForFirst) {
           _waitForFirst = false;
         } else {
-          _prev.Add(_current);
+          _prev.Add(newCurr);
           OnPropertyChanged("Average");
         }
-        _current = 0;
+      } else {
+        _current++;
       }
 
       OnPropertyChanged("Current");
+    }
+
+    bool Condition(Pack Pack) {
+      return Pack.Cards.Any(x => x.Rarity == _rarity && (_premium ? x.Premium : true));
     }
 
     public event PropertyChangedEventHandler PropertyChanged;
