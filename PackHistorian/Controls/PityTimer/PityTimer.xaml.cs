70b1694683d06a41fb16731bbe77da11301289a3
M. Kapma <marcel_kapma@web.de>
2017-08-02 00:09:43 +0200

    Implements the PityCounters into the PityCounters-Window

--- PackHistorian/Controls/PityTimer/PityTimer.xaml.cs
+++ PackHistorian/Controls/PityTimer/PityTimer.xaml.cs
@@ -11,14 +11,30 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using HearthDb.Enums;
+using MahApps.Metro.Controls;
+using PackTracker.View.Cache;
 
 namespace PackTracker.Controls.PityTimer {
   /// <summary>
   /// Interaktionslogik für PityTimer.xaml
   /// </summary>
-  public partial class PityTimer : Window {
-    public PityTimer(PackTracker.History History) {
+  public partial class PityTimer : MetroWindow {
+    PityTimerRepository _pityTimers;
+
+    public PityTimer(PackTracker.History History, PityTimerRepository PityTimers) {
       InitializeComponent();
+
+      _pityTimers = PityTimers;
+
+      dd_Packs.SelectionChanged += (sender, e) => {
+        if(e.AddedItems.Count == 1) {
+          Ep_Prev.DataContext = Ep_Label.DataContext = _pityTimers.GetPityTimer((int)e.AddedItems[0], Rarity.EPIC, false, true);
+          Leg_Prev.DataContext = Leg_Label.DataContext = _pityTimers.GetPityTimer((int)e.AddedItems[0], Rarity.LEGENDARY, false, true);
+        }
+      };
+
+      Loaded += (sender, e) => dd_Packs.DataContext = History;
     }
   }
 }
