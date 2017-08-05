da55f67802ca0263028754f6d229cff624cc72bf
M. Kapma <marcel_kapma@web.de>
2017-08-05 18:47:11 +0200

    Organize windows in WindowManager

--- PackHistorian/Plugin.cs
+++ PackHistorian/Plugin.cs
@@ -20,95 +20,11 @@ namespace PackTracker {
     IHistoryStorage _historyStorage = new XmlHistory();
     Settings _settings;
     ISettingsStorage _settingsStorage = new XmlSettings();
-    Controls.History _historyWin;
-    Controls.Statistic _statisticWin;
-    Controls.Log _logWin;
-    Controls.Search _searchWin;
-    Controls.PityTimer.PityTimer _pityWin;
+    WindowManager _windows = new WindowManager(_name);
     View.AverageCollection _averageCollection;
     View.Cache.PityTimerRepository _pityTimers;
 
     public static Version CurrentVersion { get => _version; }
-
-    Controls.History HistoryWin {
-      get {
-        if(_historyWin == null) {
-          _historyWin = new Controls.History(_history, new Controls.HistoryDatePicker(_history)) {
-            Owner = Hearthstone_Deck_Tracker.Core.MainWindow,
-          };
-          _historyWin.Closed += (sender, e) => { _historyWin = null; };
-          _historyWin.Loaded += (sender, e) => _historyWin.Title = Name + ": " + _historyWin.Title;
-        }
-
-        return _historyWin;
-      }
-    }
-
-    Controls.Statistic StatisticWin {
-      get {
-        if(_statisticWin == null) {
-          _statisticWin = new Controls.Statistic(_history) {
-            Owner = Hearthstone_Deck_Tracker.Core.MainWindow,
-          };
-          _statisticWin.Closed += (sender, e) => _statisticWin = null;
-          _statisticWin.Loaded += (sender, e) => _statisticWin.Title = Name + ": " + _statisticWin.Title;
-        }
-
-        return _statisticWin;
-      }
-    }
-
-    Controls.Log LogWin {
-      get {
-        if(_logWin == null) {
-          _logWin = new Controls.Log(_history) {
-            Owner = Hearthstone_Deck_Tracker.Core.MainWindow,
-          };
-          _logWin.Closed += (sender, e) => _logWin = null;
-          _logWin.Loaded += (sender, e) => _logWin.Title = Name + ": " + _logWin.Title;
-        }
-
-        return _logWin;
-      }
-    }
-
-    Controls.Settings.Settings CreateSettingsWin() {
-      Controls.Settings.Settings Win = new Controls.Settings.Settings(_settings) {
-        Owner = Hearthstone_Deck_Tracker.Core.MainWindow
-      };
-      Win.Closed += (sender, e) => _settingsStorage.Store(_settings);
-      Win.Title = Name + ": " + Win.Title;
-
-      return Win;
-    }
-
-    Controls.Search SearchWin {
-      get {
-        if(_searchWin == null) {
-          _searchWin = new Controls.Search(_history) {
-            Owner = Hearthstone_Deck_Tracker.Core.MainWindow,
-          };
-          _searchWin.Closed += (sender, e) => _searchWin = null;
-          _searchWin.Loaded += (sender, e) => _searchWin.Title = Name + ": " + _searchWin.Title;
-        }
-
-        return _searchWin;
-      }
-    }
-
-    Controls.PityTimer.PityTimer PityWin {
-      get {
-        if(_pityWin == null) {
-          _pityWin = new Controls.PityTimer.PityTimer(_history, _pityTimers) {
-            Owner = Hearthstone_Deck_Tracker.Core.MainWindow,
-          };
-          _pityWin.Closed += (sender, e) => _pityWin = null;
-          _pityWin.Loaded += (sender, e) => _pityWin.Title = Name + ": " + _pityWin.Title;
-        }
-
-        return _pityWin;
-      }
-    }
 
     public Plugin() {
       _watcher = new PackWatcher();
@@ -157,11 +73,11 @@ namespace PackTracker {
     public MenuItem MenuItem {
       get {
         Controls.Menu Menu = new Controls.Menu();
-        Menu.mnu_History.Click += (sender, e) => { HistoryWin.Show(); HistoryWin.Focus(); };
-        Menu.mnu_Statistic.Click += (sender, e) => { StatisticWin.Show(); StatisticWin.Focus(); };
-        Menu.mnu_Log.Click += (sender, e) => { LogWin.Show(); LogWin.Focus(); };
-        Menu.mnu_Search.Click += (sender, e) => { SearchWin.Show(); SearchWin.Focus(); };
-        Menu.mnu_PityTimers.Click += (sender, e) => { PityWin.Show(); PityWin.Focus(); };
+        Menu.mnu_History.Click += (sender, e) => _windows.ShowHistoryWin(_history);
+        Menu.mnu_Statistic.Click += (sender, e) => _windows.ShowStatisticWin(_history);
+        Menu.mnu_Log.Click += (sender, e) => _windows.ShowLogWin(_history);
+        Menu.mnu_Search.Click += (sender, e) => _windows.ShowSearchWin(_history);
+        Menu.mnu_PityTimers.Click += (sender, e) => _windows.ShowPityWin(_history, _pityTimers);
 
         return Menu;
       }
@@ -178,7 +94,7 @@ namespace PackTracker {
     }
 
     public void OnButtonPress() {
-      CreateSettingsWin().ShowDialog();
+      _windows.ShowSettingsWin(_settings, _settingsStorage);
     }
 
     public void OnLoad() {
@@ -201,14 +117,7 @@ namespace PackTracker {
         };
         bwCheck.RunWorkerCompleted += (sender, e) => {
           if((bool?)e.Result == true) {
-            Controls.Settings.Settings Settings = CreateSettingsWin();
-            foreach(var Item in Settings.lb_tabs.Items) {
-              if(Item is Controls.Settings.Update) {
-                Settings.lb_tabs.SelectedItem = Item;
-                Settings.ShowDialog();
-                break;
-              }
-            }
+            _windows.ShowSettingsWin(_settings, _settingsStorage, typeof(Controls.Settings.Update));
           }
         };
         bwCheck.RunWorkerAsync();
---
+++ PackHistorian/WindowManager.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using PackTracker.Controls;
+using PackTracker.Storage;
+using PackTracker.View.Cache;
+
+namespace PackTracker {
+
+  class WindowManager {
+    string _name;
+    Window _pityWin, _statisticWin, _historyWin, _logWin, _searchWin;
+
+    public WindowManager(string name) {
+      _name = name;
+    }
+
+    public void ShowPityWin(History History, PityTimerRepository PityTimers) {
+      if(_pityWin == null) {
+        _pityWin = new Controls.PityTimer.PityTimer(History, PityTimers) {
+          Owner = Hearthstone_Deck_Tracker.Core.MainWindow,
+        };
+        _pityWin.Closed += (sender, e) => _pityWin = null;
+        _pityWin.Loaded += (sender, e) => _pityWin.Title = _name + ": " + _pityWin.Title;
+
+        _pityWin.Show();
+      }
+
+      _pityWin.Focus();
+    }
+
+    public void ShowStatisticWin(History History) {
+      if(_statisticWin == null) {
+        _statisticWin = new Statistic(History) {
+          Owner = Hearthstone_Deck_Tracker.Core.MainWindow,
+        };
+        _statisticWin.Closed += (sender, e) => _statisticWin = null;
+        _statisticWin.Loaded += (sender, e) => _statisticWin.Title = _name + ": " + _statisticWin.Title;
+
+        _statisticWin.Show();
+      }
+
+      _statisticWin.Focus();
+    }
+
+    public void ShowHistoryWin(History History) {
+      if(_historyWin == null) {
+        _historyWin = new Controls.History(History, new HistoryDatePicker(History)) {
+          Owner = Hearthstone_Deck_Tracker.Core.MainWindow,
+        };
+        _historyWin.Closed += (sender, e) => { _historyWin = null; };
+        _historyWin.Loaded += (sender, e) => _historyWin.Title = _name + ": " + _historyWin.Title;
+
+      _historyWin.Show();
+      }
+
+      _historyWin.Focus(); ;
+    }
+
+    public void ShowLogWin(History History) {
+      if(_logWin == null) {
+        _logWin = new Log(History) {
+          Owner = Hearthstone_Deck_Tracker.Core.MainWindow,
+        };
+        _logWin.Closed += (sender, e) => _logWin = null;
+        _logWin.Loaded += (sender, e) => _logWin.Title = _name + ": " + _logWin.Title;
+
+        _logWin.Show();
+      }
+
+      _logWin.Focus();
+    }
+
+    public void ShowSearchWin(History History) {
+      if(_searchWin == null) {
+        _searchWin = new Search(History) {
+          Owner = Hearthstone_Deck_Tracker.Core.MainWindow,
+        };
+        _searchWin.Closed += (sender, e) => _searchWin = null;
+        _searchWin.Loaded += (sender, e) => _searchWin.Title = _name + ": " + _searchWin.Title;
+
+        _searchWin.Show();
+      }
+
+      _searchWin.Focus();
+    }
+
+    public void ShowSettingsWin(Settings Settings, ISettingsStorage SettingsStorage, Type PreSelection = null) {
+      Controls.Settings.Settings Win = new Controls.Settings.Settings(Settings) {
+        Owner = Hearthstone_Deck_Tracker.Core.MainWindow
+      };
+      Win.Closed += (sender, e) => SettingsStorage.Store(Settings);
+      Win.Title = _name + ": " + Win.Title;
+
+      if(PreSelection != null) {
+        foreach(var Item in Win.lb_tabs.Items) {
+          if(Item.GetType() == PreSelection) {
+            Win.lb_tabs.SelectedItem = Item;
+            break;
+          }
+        }
+      }
+
+      Win.ShowDialog();
+    }
+
+
+  }
+}
