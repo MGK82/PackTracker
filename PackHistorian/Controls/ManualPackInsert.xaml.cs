03734b30aa3cf2b6802961429337faac43c95ce9
Luca Culzoni <culzluca@libero.it>
2019-10-15 20:07:26 +0200

    First proposal for manual pack insert window

---
+++ PackHistorian/Controls/ManualPackInsert.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Collections.Specialized;
+using PackTracker.Entity;
+
+namespace PackTracker.Controls
+{
+    /// <summary>
+    /// Interaktionslogik für ManualPackInsert.xaml
+    /// </summary>
+    public partial class ManualPackInsert
+    {
+        public ManualPackInsert(PackTracker.History History)
+        {
+            DataContext = new View.ManualPackInsert(History);
+
+            InitializeComponent();
+        }
+    }
+}
